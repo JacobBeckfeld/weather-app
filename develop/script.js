a9fe43e0e6539c653b9c775a509a30e15614a10e
JacobBeckfeld <jacbeck456@gmail.com>
2021-06-23 15:49:13 -0500

    added the time conveter that will convert to am pm time

--- develop/script.js
+++ develop/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     //click event for city search
     $("#search-btn, #searched").on("click", function(){
-        // console.log(this);
+        console.log(this);
         let clickBtn = $(event.target)[0];
         let city = "";
         if (clickBtn.id === "search") {
@@ -16,11 +16,25 @@ $(document).ready(function(){
         updateLocalStorage(city);
         getCurrentWeather(city);
         getForcastWeather(city);
+        console.log(this)
     })
 
+    function convertDate(UNIXtimestamp){
+        let convertDate = "";
+        let a = new Date(UNIXtimestamp * 1000);
+        let months = ["Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let year = a.getFullYear();
+        let month = months[a.getMonth()];
+        let date = a.getDate();
+        convertDate = month + " " + date + ", " + year;
+        return convertDate;
+        console.log(this)
+    }
 
+    
 
 })
+
 
 
 //create a function that will connect to the api
