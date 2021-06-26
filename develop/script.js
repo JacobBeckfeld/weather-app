ae8c1bbb04ba098688dd589ab542e30a296d219b
JacobBeckfeld <jacbeck456@gmail.com>
2021-06-25 21:32:33 -0500

    tried getting the page to populate but its not working

--- develop/script.js
+++ develop/script.js
@@ -1,8 +1,8 @@
-function startPage(){}
+function startPage(){
 var userInput = document.getElementById("user-input");
 var searchBtn = document.getElementById("search-btn");
 var city = document.getElementById("city");
-var icon = document.getElementById("icon");
+var weatherIcon = document.getElementById("icon");
 var temp = document.getElementById("temp");
 var humidity = document.getElementById("humidity");
 var windSpeed = document.getElementById("windspeed");
@@ -10,19 +10,33 @@ var url = "https://api.openweathermap.org/data/2.5/weather?q=";
 var appid = "&appid=";
 var APIKey = "a2198b8b1a95c35dfd7fa87eadfe55b1";
 
+
 function currentWeather(cityName) {
     var forcast1 = url + cityName +appid +APIKey;
     console.log(forcast1)
     fetch(forcast1)
     .then(function(response){
         console.log(response);
-        return response.json()
-        
-    }
-    
-    ).then(function(data){
-        console.log(data)
+        var todayDate = new Date(response.data.dt*1000);
+        console.log(todayDate);
+        var day = todayDate.getData();
+        var month = todayDate.getMonth() + 1;
+        var year = todayDate.getFullYear();
+        city.innerHTML = response.data.cityName + "(" + month + "/" + day + "/" + year + ") ";
+        var picture = response.data.weather[0].icon;
+        weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + picture + "@2x.png");
+        weatherIcon.setAttribute("alt", response.data.weather[0].description);
+        temp.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&#176F";
+        humidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        windSpeed.innerHTML = "Wind Speed:" + response.data.wind.speed + " MPH";
+        var latatude = response.data.coord.lat;
+        var longitude = response.data.coord.lon;
+        var UVURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latatude + "&lon=" + longitude + "&appid+" + APIKey + "&cnt=1";
+
     })
+    
+    
+}
+    currentWeather("seattle");
 }
 
-currentWeather("Seattle")
