aedf64d8ff326a86faf2750ae665e159533ee45e
JacobBeckfeld <jacbeck456@gmail.com>
2021-08-19 17:43:30 -0500

    finished some styling and the cards

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is a simple app that will give the forecast for the day and the next five days.
+
+![screenshot](./develop/Weather.png)
--- develop/script.js
+++ develop/script.js
@@ -36,6 +36,12 @@ let tempFive = document.getElementById("tempFive");
 let humFive = document.getElementById("humFive")
 let windFive = document.getElementById("speedFive")
 
+let today = moment()
+$("#day1").text(today.format("dddd MMMM Do"))
+$("#day2").text(today.add(1, "days").format("dddd MMMM Do"));
+$("#day3").text(today.add(2, "days").format("dddd MMMM Do"));
+$("#day4").text(today.add(3, "days").format("dddd MMMM Do"));
+$("#day5").text(today.add(4, "days").format("dddd MMMM Do"));
 
 
 function currentWeather() {
@@ -85,29 +91,48 @@ function forcast(){
     })
     .then(function(data){
         console.log(data)
-    })
+    
     let temp1 = data.list[0].main.temp
     let hum1 = data.list[0].main.humidity
     let wind1 = data.list[0].wind.speed
 
-    let temp2 = data.list[0].main.temp
-    let hum2 = data.list[0].main.humidity
-    let wind2 = data.list[0].wind.speed
+    let temp2 = data.list[8].main.temp
+    let hum2 = data.list[8].main.humidity
+    let wind2 = data.list[8].wind.speed
     
-    let temp3 = data.list[0].main.temp
-    let hum3 = data.list[0].main.humidity
-    let wind3 = data.list[0].wind.speed
+    let temp3 = data.list[16].main.temp
+    let hum3 = data.list[16].main.humidity
+    let wind3 = data.list[16].wind.speed
     
-    let temp4 = data.list[0].main.temp
-    let hum4 = data.list[0].main.humidity
-    let wind4 = data.list[0].wind.speed
+    let temp4 = data.list[24].main.temp
+    let hum4 = data.list[24].main.humidity
+    let wind4 = data.list[24].wind.speed
     
-    let temp5 = data.list[0].main.temp
-    let hum5 = data.list[0].main.humidity
-    let wind5 = data.list[0].wind.speed
+    let temp5 = data.list[32].main.temp
+    let hum5 = data.list[32].main.humidity
+    let wind5 = data.list[32].wind.speed
 
+    tempOne.textContent = "Temp: " + temp1
+    humOne.textContent = "Humidity: " + hum1
+    windOne.textContent = "Windspeed: " + wind1
 
+    tempTwo.textContent = "Temp: " + temp2
+    humTwo.textContent = "Humidity: " + hum2
+    windTwo.textContent = "Windspeed: " + wind2
 
+    tempThree.textContent = "Temp: " + temp3
+    humThree.textContent = "Humidity: " + hum3
+    windThree.textContent = "Windspeed: " + wind3
+
+    tempFour.textContent = "Temp: " + temp4
+    humFour.textContent = "Humidity: " + hum4
+    windFour.textContent = "Windspeed: " + wind4
+
+    tempFive.textContent = "Temp: " + temp5
+    humFive.textContent = "Humidity: " + hum5
+    windFive.textContent = "Windspeed: " + wind5
+
+    })
 }
 
 
@@ -116,5 +141,5 @@ function startPage(){
     currentWeather("seattle")
     forcast("Seattle")
 }
-startPage()
 searchBtn.addEventListener("click", startPage)
+startPage()
